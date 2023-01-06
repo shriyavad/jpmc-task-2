634e6ff9062053efc671eab5e9031d9eef760afd
shriyavad <84842156+shriyavad@users.noreply.github.com>
2023-01-06 21:06:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,20 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
-
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
- * The parent element of the react app.
- * It renders title, button and Graph react element.
- */
-class App extends Component<{}, IState> {
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
+@@ -22,26 +23,39 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -30,6 +24,9 @@ class App extends Component<{}, IState> {
    */
   renderGraph() {
     return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -37,11 +34,24 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
+    let x=0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState({ 
+        data: serverResponds,
+        showGraph: true,
+      });
     });
   }
+    x++;
+    if(x>1000){
+      clearInterval(interval);
+    }
+  },100);
+}
 
   /**
    * Render the App react component
@@ -65,10 +75,3 @@ class App extends Component<{}, IState> {
           <div className="Graph">
             {this.renderGraph()}
           </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default App;
